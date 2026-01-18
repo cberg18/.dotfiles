a51dc4ce388633fc12aa5fbad1505a72bbc3a5a6
Cory Berg <cberg18@gmail.com>
2026-01-18 09:38:04 -0700

    updates

--- setup_remote.sh
+++ setup_remote.sh
@@ -6,6 +6,10 @@ if [ -z "$1" ]; then
 fi
 
 REMOTE_HOST=$1
+REMOTE_GIT_USER="cberg18"
+REMOTE_GIT_URL="github.com"
+REMOTE_GIT_SSH_URL="git@$REMOTE_GIT_URL"
+REMOTE_GIT_HTTPS_URL="https://$REMOTE_GIT_URL"
 
 # Copy public key to the remote host for access
 echo "=> Copying public key to $REMOTE_HOST..."
@@ -13,7 +17,7 @@ ssh-copy-id -i ~/.ssh/id_ed25519 "$REMOTE_HOST"
 
 # Test SSH connectivity to the git server on the remote host
 echo "=> Testing git access on $REMOTE_HOST..."
-if ssh "$REMOTE_HOST" "ssh -T -o StrictHostKeyChecking=no git@git.hcmsgroup.com"; then
+if ssh "$REMOTE_HOST" "ssh -T -o StrictHostKeyChecking=no $REMOTE_GIT_SSH_URL"; then
     echo "=> Git SSH access confirmed."
 else
     echo "! Git SSH access failed. Configuring private key..."
@@ -25,12 +29,12 @@ else
     ssh "$REMOTE_HOST" "chmod 700 ~/.ssh && chmod 600 ~/.ssh/id_rsa"
 
     # Configure SSH config
-    ssh "$REMOTE_HOST" "echo 'Host git.hcmsgroup.com
+    ssh "$REMOTE_HOST" "echo 'Host $REMOTE_GIT_URL
   IdentityFile ~/.ssh/id_rsa
   User git' >> ~/.ssh/config && chmod 600 ~/.ssh/config"
 
     # Configure git to use SSH instead of HTTPS for this domain
-    ssh "$REMOTE_HOST" "git config --global url.\"git@git.hcmsgroup.com:\".insteadOf \"https://git.hcmsgroup.com/\""
+    ssh "$REMOTE_HOST" "git config --global url.\"$REMOTE_GIT_SSH_URL:\".insteadOf \"$REMOTE_GIT_HTTPS_URL/\""
 fi
 
 # Check if .dotfiles is a git repo, clone if not
@@ -38,7 +42,7 @@ echo "# Checking .dotfiles repository..."
 ssh "$REMOTE_HOST" "if [ ! -d ~/.dotfiles/.git ]; then
     echo '=> Cloning .dotfiles...'
     rm -rf ~/.dotfiles
-    git clone https://git.hcmsgroup.com/bergc/.dotfiles ~/.dotfiles
+    git clone $REMOTE_GIT_SSH_URL/$REMOTE_GIT_USER/.dotfiles ~/.dotfiles
 else
     echo '# .dotfiles repository already exists.'
 fi"
