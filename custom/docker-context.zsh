5aaa3e4347ead5b5fdf0005dc05e2b4ce78497d5
Cory Berg <cberg18@gmail.com>
2026-01-20 17:26:56 -0700

    fix extra quote

--- custom/docker-context.zsh
+++ custom/docker-context.zsh
@@ -1,7 +1,7 @@
 docker_prompt_info(){
     if command -v docker >/dev/null 2>&1; then
         export DOCKER_CONTEXT=$(docker context ls | grep \* | awk '{print $1}')
-        docker_icon='''
+        docker_icon=''
         ZSH_THEME_DOCKER_CONTEXT_PREFIX="$docker_icon  context: "
         ZSH_THEME_DOCKER_CONTEXT="%{$fg[green]%}$DOCKER_CONTEXT%{$reset_color%}"
         ZSH_THEME_DOCKER_CONTEXT_SUFFIX="%{$reset_color%}"
